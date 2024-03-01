fb59527eea4b5bf6a1dc1e6ac7cb63163e75cf1e
cocktailpeanut <cocktailpeanuts@proton.me>
2024-03-01 11:06:53 -0500

    init

---
+++ install.js
@@ -0,0 +1,51 @@
+const config = require("./config.js")
+const pre = require("./pre.js")
+module.exports = async (kernel) => {
+  let script = {
+    run: [{
+      method: "shell.run",
+      params: {
+        message: [
+          "git clone https://github.com/naver/dust3r --recursive app",
+        ]
+      }
+    }, {
+      method: "shell.run",
+      params: {
+        venv: "env",
+        path: "app",
+        message: [
+          "pip install -r requirements.txt"
+        ],
+      }
+    }, {
+      method: "fs.share",
+      params: {
+        venv: "app/env"
+      }
+    }, {
+      method: "fs.share",
+      params: {
+        drive: {
+          checkpoints: "app/checkpoints" 
+        }
+      }
+    }, {
+      method: "fs.download",
+      params: {
+        uri: "https://download.europe.naverlabs.com/ComputerVision/DUSt3R/DUSt3R_ViTLarge_BaseDecoder_512_dpt.pth ",
+        dir: "app/checkpoints"
+      }
+    }, {
+      method: "notify",
+      params: {
+        html: "Click the 'start' tab to get started!"
+      }
+    }]
+  }
+  let pre_command = pre(config, kernel)
+  if (pre_command) {
+    script.run[1].params.message = [pre_command].concat(script.run[1].params.message)
+  }
+  return script
+}
---
+++ start.js
@@ -0,0 +1,37 @@
+module.exports = async (kernel) => {
+  let device
+  if (kernel.gpu === 'nvidia') {
+    device = 'cuda'
+  } else if (kernel.platform === 'darwin' && kernel.arch === 'arm64') {
+    device = 'mps'
+  } else {
+    device = 'cpu'
+  }
+  let script = {
+    daemon: true,
+    run: [{
+      method: "shell.run",
+      params: {
+        path: "app",
+        venv: "env",
+        env: { "PYTORCH_ENABLE_MPS_FALLBACK": "1" },
+        message: [
+          "python3 demo.py --device " + device + " --weights checkpoints/DUSt3R_ViTLarge_BaseDecoder_512_dpt.pth"
+        ],
+        on: [{ "event": "/http:\/\/[0-9.:]+/", "done": true }]
+      }
+    }, {
+      "method": "local.set",
+      "params": {
+        "url": "{{input.event[0]}}"
+      }
+    }, {
+      "method": "proxy.start",
+      "params": {
+        "uri": "{{local.url}}",
+        "name": "Local Sharing"
+      }
+    }]
+  }
+  return script
+}
